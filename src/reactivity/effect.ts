5b3fc656a94f864e3e7e64194de3fe8831912594
Zhuangyupeng <2415580451@qq.com>
2022-06-27 17:39:04 +0800

    [+] 初始化项目

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,65 @@
+import { extend } from "../shared";
+
+let activeEffect: ReactiveEffect;
+const targetMap = new Map();
+class ReactiveEffect {
+    protected _fn: any;
+    public scheduler: Function | undefined;
+    public deps: any[] = [];
+    constructor(fn, scheduler?: Function) {
+        this._fn = fn;
+        this.scheduler = scheduler;
+    }
+    public run() {
+        activeEffect = this;
+        return this._fn();
+    }
+
+    public stop() {
+        cleanupEffect(this);
+    }
+}
+
+function cleanupEffect(effect: ReactiveEffect) {
+    effect.deps.forEach(dep => {
+        dep.delete(effect);
+    });
+    // effect.deps.length = 0;
+}
+// 收集依赖
+export function track(target, key) {
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+        depsMap = new Map();
+        targetMap.set(target, depsMap);
+    }
+    let dep = depsMap.get(key);
+    if (!dep) {
+        dep = new Set();
+        depsMap.set(key, dep);
+    }
+    if (dep.has(activeEffect)) return;
+    activeEffect.deps.push(dep);
+    dep.add(activeEffect);
+}
+// 触发依赖
+export function trigger(target, key) {
+    let depsMap = targetMap.get(target);
+    if(!depsMap) return;
+    let dep = depsMap.get(key);
+    if(!dep) return;
+    for (const effect of dep) {
+        effect.scheduler ? effect.scheduler() : effect.run();
+    }
+}
+export function effect(fn, options: any = {}) {
+    const _effect = new ReactiveEffect(fn, options.scheduler);
+    _effect.run();
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
+
+export function stop(runner) {
+    runner.effect.stop();
+}
---
+++ src/reactivity/test/effect.spec.ts
@@ -0,0 +1,94 @@
+import { effect, stop } from "../effect";
+import { reactive } from "../reactive";
+describe('effect', () => {
+    it('happy path', () => {
+        const user = reactive({
+            age: 10
+        });
+        let newAge = 0;
+        effect(() => {
+            newAge = user.age + 1;
+        });
+        expect(newAge).toBe(11);
+        user.age++;
+        expect(newAge).toBe(12);
+    });
+
+    it('should return runner when call effect', () => {
+        let foo = 10;
+        const res = effect(() => {
+            foo++;
+            return 'foo';
+        });
+        expect(foo).toBe(11);
+        const r = res();
+        expect(foo).toBe(12);
+        expect(r).toBe('foo');
+    });
+
+    it("scheduler", () => {
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({ foo: 1 });
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            { scheduler }
+        );
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        // should be called on first trigger
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        // // should not run yet
+        expect(dummy).toBe(1);
+        // // manually run
+        run();
+        // // should have run
+        expect(dummy).toBe(2);
+    });
+
+    it("stop", () => {
+        let dummy;
+        const obj = reactive({ prop: 1 });
+        const runner = effect(() => {
+            dummy = obj.prop;
+        });
+        obj.prop = 2;
+        expect(dummy).toBe(2);
+        // runner.effect
+        console.log('-----', runner.effect.deps[0].size);
+        stop(runner);
+        console.log('-----', runner.effect.deps[0].size);
+
+        // obj.prop = 3;
+        obj.prop++;
+        expect(dummy).toBe(2);
+
+        // stopped effect should still be manually callable
+        runner();
+        expect(dummy).toBe(3);
+    });
+
+    // it("onStop", () => {
+    //     const obj = reactive({
+    //         foo: 1,
+    //     });
+    //     const onStop = jest.fn();
+    //     let dummy;
+    //     const runner = effect(
+    //         () => {
+    //             dummy = obj.foo;
+    //         },
+    //         {
+    //             onStop,
+    //         }
+    //     );
+    //     stop(runner);
+    //     expect(onStop).toBeCalledTimes(1);
+    // });
+});
